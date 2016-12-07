34221b27476bc2e9d2b8f1a4dd1ee1541e42ac0d
Renzo Lucioni <renzo@lucioni.xyz>
2016-12-07 00:29:24 -0500

    Add a function for getting all card data

--- cards.py
+++ cards.py
@@ -36,15 +36,32 @@ logger = logging.getLogger()
 with open('settings.yml') as f:
     settings = yaml.load(f)
 
+
 mashape_api_root = settings['mashape_api']['root']
 mashape_api_key = settings['mashape_api']['key']
 
-# TODO: Use https://pypi.python.org/pypi/slumber/0.7.1
-all_cards_url = urljoin(mashape_api_root, 'cards')
-headers = {
-    'X-Mashape-Key': mashape_api_key,
-}
-response = requests.get(all_cards_url, headers=headers)
 
-# Data is a dict of lists. The keys are card set names (e.g., 'Mean Streets of Gadgetzan').
-data = response.json()
+def get_all_cards():
+    """
+    Data is a dict of lists. The keys are card set names (e.g., 'Mean Streets of Gadgetzan').
+    The card set lists may contain minions, spells, enchantments, and more.
+
+    >>> data['Mean Streets of Gadgetzan'][0]
+    {
+        'artist': 'Paul Mafayon',
+        'type': 'Enchantment',
+        'locale': 'enUS',
+        'text': 'Increased Attack.',
+        'cardSet': 'Mean Streets of Gadgetzan',
+        'name': 'Cut Off',
+        'cardId': 'CFM_658e',
+        'playerClass': 'Neutral'
+    }
+    """
+    # TODO: Use https://pypi.python.org/pypi/slumber/0.7.1
+    all_cards_url = urljoin(mashape_api_root, 'cards')
+    headers = {
+        'X-Mashape-Key': mashape_api_key,
+    }
+    response = requests.get(all_cards_url, headers=headers)
+    return response.json()
