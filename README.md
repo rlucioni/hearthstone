3983df2591afacfa35de64ae2a89c076fa8d3943
Renzo Lucioni <renzo@lucioni.xyz>
2016-12-07 00:09:36 -0500

    Add script for getting all card data

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # hearthstone-cards
 
 Tools for studying Hearthstone cards. Data is provided by a [third-party API](http://hearthstoneapi.com/).
+
+Create a `settings.yml` file at the root of the repo and put your Mashape application key in it. See `settings.yml.example` for an example.
---
+++ cards.py
@@ -0,0 +1,47 @@
+import logging
+from logging.config import dictConfig
+from urllib.parse import urljoin
+
+import requests
+import yaml
+
+
+# Configure logging.
+dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s %(levelname)s %(process)d [%(filename)s:%(lineno)d] - %(message)s',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False
+        },
+    },
+})
+logger = logging.getLogger()
+
+
+with open('settings.yml') as f:
+    settings = yaml.load(f)
+
+mashape_api_root = settings['mashape_api']['root']
+mashape_api_key = settings['mashape_api']['key']
+
+# TODO: Use https://pypi.python.org/pypi/slumber/0.7.1
+all_cards_url = urljoin(mashape_api_root, 'cards')
+headers = {
+    'X-Mashape-Key': mashape_api_key,
+}
+all_card_data = requests.get(all_cards_url, headers=headers)
